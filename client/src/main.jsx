8458ef5bfa482fd1e591457efab7ab0bf7b182a4
Nolan <nolannaphys@gmail.com>
2023-08-28 18:13:49 -0400

    reformated code in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,29 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-
-import 'bootstrap/dist/css/bootstrap.min.css'
-
-import './index.css'
-
+import { React } from 'react';
+import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
-import App from './App.jsx'
-import Error from './pages/Error'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Maze from './pages/Maze'
-import Profile from './pages/Profile'
+import App from './App.jsx';
+import Error from './pages/Error';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Maze from './pages/Maze';
+import Profile from './pages/Profile';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
     children: [
-      { path: '/', element: <Home /> },
-      { path: '/login', element: <Login /> },
-      { path: '/maze', element: <Maze /> },
-      { path: '/profile', element: <Profile /> },
-      { path: '*', element: <Error /> },
+      { 
+        path: '/', 
+        element: <Home /> 
+      },
+      { 
+        path: '/login', 
+        element: <Login /> 
+      },
+      { 
+        path: '/maze', 
+        element: <Maze /> 
+      },
+      { 
+        path: '/profile', 
+        element: <Profile /> 
+      },
+      { 
+        path: '*', 
+        element: <Error /> 
+      },
     ],
   }
 ]);
